5b4f65d1fd054614169232cf50f70cef76297e88
Lo7u5 <40316422+Lo7u5@users.noreply.github.com>
2023-12-16 14:10:47 +0000

    add test newOrderHandler

--- 36UN/orders/pkg/api/api_test.go
+++ 36UN/orders/pkg/api/api_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -31,5 +32,33 @@ func TestAPI_ordersHandler(t *testing.T) {
 	const wantLen = 1
 	if len(data) != wantLen {
 		t.Fatalf("получено %d записей, ожидалось %d", len(data), wantLen)
+	}
+}
+
+func TestAPI_newOrderHandler(t *testing.T) {
+	dbase := db.New()
+	api := New(dbase)
+	order := db.Order{}
+	orderJson, err := json.Marshal(order)
+	if err != nil {
+		t.Fatalf("не удалось сериализовать объект заказа: %v", err)
+	}
+	req := httptest.NewRequest(http.MethodPost, "/orders", bytes.NewBuffer(orderJson))
+	rr := httptest.NewRecorder()
+	api.r.ServeHTTP(rr, req)
+	if !(rr.Code == http.StatusOK) {
+		t.Errorf("код неверен: получили %d, а хотели %d", rr.Code, http.StatusOK)
+	}
+	b, err := io.ReadAll(rr.Body)
+	if err != nil {
+		t.Fatalf("не удалось раскодировать ответ сервера: %v", err)
+	}
+	var id int
+	err = json.Unmarshal(b, &id)
+	if err != nil {
+		t.Fatalf("не удалось раскодировать ответ сервера: %v", err)
+	}
+	if id != 1{
+		t.Fatalf("получен %d id, ожидалось %d", id, 2)
 	}
 }
